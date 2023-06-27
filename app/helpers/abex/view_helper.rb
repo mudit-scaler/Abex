6451c494d52141d5170bb853347b3f45c37823a4
Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>
2023-06-27 13:06:06 +0530

    feat: updte
    Signed-off-by: Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>

--- app/helpers/abex/view_helper.rb
+++ app/helpers/abex/view_helper.rb
@@ -2,12 +2,14 @@ module Abex
   # Module to allow shorthand calls for Abex components
   module ViewHelper
   
-    def abex_switch(flag_key:, variant:)
-      render "abex/context_component", flag_key: flag_key, variant: variant
-    end
-
-    def abex_case(variant:)
-      render "abex/component", variant: variant
+    class << self
+      def abex_switch(flag_key:, variant:)
+        render "abex/context_component", flag_key: flag_key, variant: variant
+      end
+  
+      def abex_case(variant:)
+        render "abex/component", variant: variant
+      end
     end
   end
 end
