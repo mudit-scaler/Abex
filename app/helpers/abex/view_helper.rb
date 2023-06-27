3f6188b3fe0746bb488779dee9ce4bc243cdca36
Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>
2023-06-28 03:39:09 +0530

    fix: nit
    Signed-off-by: Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>

--- app/helpers/abex/view_helper.rb
+++ app/helpers/abex/view_helper.rb
@@ -1,28 +1,16 @@
 module Abex
   # Module to allow shorthand calls for Abex components
   module ViewHelper
-  
-    # def abex_switch(flag_key:, variant:, &block)
-    #   render "abex/context_component", flag_key: flag_key, variant: variant do
-    #     capture(&block)
-    #   end
-    # end
+    ABEX_HELPERS = {
+      switch: "context_component",
+      case: "component"
+    }.freeze
 
-    # def abex_case(variant:, &block)
-    #   render "abex/component", variant: variant do
-    #     capture(&block)
-    #   end
-    # end
-
-    define_method "abex_switch" do |*args, **kwargs, &block|
-      render "abex/context_component", *args, **kwargs do
-        capture(&block)
-      end
-    end
-
-    define_method "abex_case" do |*args, **kwargs, &block|
-      render "abex/component", *args, **kwargs do
-        capture(&block)
+    ABEX_HELPERS.each do |name, component_path|
+      define_method "abex_#{name}" do |*args, **kwargs, &block|
+        render "abex/#{component_path}", *args, **kwargs do
+          capture(&block)
+        end
       end
     end
   end
