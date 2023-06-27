23422ecc179a3bff2ad177fb22fe4980dfe73ece
Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>
2023-06-28 03:31:38 +0530

    fix: changing view_helper methods
    Signed-off-by: Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>

--- app/helpers/abex/view_helper.rb
+++ app/helpers/abex/view_helper.rb
@@ -2,14 +2,26 @@ module Abex
   # Module to allow shorthand calls for Abex components
   module ViewHelper
   
-    def abex_switch(flag_key:, variant:, &block)
-      render "abex/context_component", flag_key: flag_key, variant: variant do
+    # def abex_switch(flag_key:, variant:, &block)
+    #   render "abex/context_component", flag_key: flag_key, variant: variant do
+    #     capture(&block)
+    #   end
+    # end
+
+    # def abex_case(variant:, &block)
+    #   render "abex/component", variant: variant do
+    #     capture(&block)
+    #   end
+    # end
+
+    define_method "abex_switch" do |*args, **kwargs, &block|
+      render "abex/context_component", *args, **kwargs do
         capture(&block)
       end
     end
 
-    def abex_case(variant:, &block)
-      render "abex/component", variant: variant do
+    define_method "abex_case" do |*args, **kwargs, &block|
+      render "abex/component", *args, **kwargs do
         capture(&block)
       end
     end
