87c488ffb9689e8ab31269db5f5e3877a838a8de
Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>
2023-06-27 14:37:42 +0530

    fix: added block
    Signed-off-by: Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>

--- app/helpers/abex/view_helper.rb
+++ app/helpers/abex/view_helper.rb
@@ -2,8 +2,10 @@ module Abex
   # Module to allow shorthand calls for Abex components
   module ViewHelper
   
-    def abex_switch(flag_key:, variant:)
-      render "abex/context_component", flag_key: flag_key, variant: variant
+    def abex_switch(&block, flag_key:, variant:)
+      render "abex/context_component", flag_key: flag_key, variant: variant do
+        capture(&block)
+      end
     end
 
     def abex_case(variant:)
