be23eab899949282549dd9dc5abf3f9bce16ccab
Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>
2023-07-10 03:50:47 +0530

    fix: try something random
    Signed-off-by: Mudit Dubey <99174849+mudit-scaler@users.noreply.github.com>

--- app/helpers/abex/view_helper.rb
+++ app/helpers/abex/view_helper.rb
@@ -6,10 +6,16 @@ module Abex
       case: "component"
     }.freeze
 
+    after_action :jijih, only: %i[abex_switch]
+
     ABEX_HELPERS.each do |name, component_path|
       define_method "abex_#{name}" do |*args, **kwargs, &block|
         render "abex/#{component_path}", *args, **kwargs, &block
       end
+    end
+
+    define_method "jijih" do 
+      debugger
     end
   end
 end
